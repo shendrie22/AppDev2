37c6d7be19b393a73d17a1e0e11253bb66af8076
shendrie22 <sherlinedeguzman3@gmail.com>
2024-04-09 12:23:32 +0800

    activity 2 branch has changes

--- my_first_app/app/Http/Controllers/UserController.php
+++ my_first_app/app/Http/Controllers/UserController.php
@@ -11,7 +11,35 @@ class UserController extends Controller
      */
     public function index()
     {
+        $data = [
+            [
+                'id' => 1,
+                'name' => 'Product 1',
+                'price' => 10.99,
+                'description' => 'This is product 1 description.'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Product 2',
+                'price' => 19.99,
+                'description' => 'This is product 2 description.'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Product 3',
+                'price' => 29.99,
+                'description' => 'This is product 3 description.'
+            ],
+            [
+                'id' => 4,
+                'name' => 'Product 4',
+                'price' => 39.99,
+                'description' => 'This is product 4 description.'
+            ]
+        ];
 
+        return response()->json($data);
+    
     }
 
     /**
@@ -19,7 +47,7 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        return 'sherline panget';
+        //
     }
 
     /**
--- my_first_app/routes/api.php
+++ my_first_app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -16,24 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
+Route::middleware('extract.token')->group(function (){
+    Route::get('/users', [UserController::class, 'index']);
+    Route::patch('/users/{user}', [PostController::class, 'update']);
 
-// Route::get('/greet', function () {
-//     return 'Hello World';
-// });
-
-// route::post('/sherline', [UserController::class, 'store']);
-
-
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
-// Route::get('/products/{product}', [ProductController::class, 'show']);
-// Route::put('/products/{product}', [ProductController::class, 'update']);
-// Route::delete('/products/{product}', [ProductController::class, 'destroy']);
-
-Route::apiResource('/products', ProductController::class);
-
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::put('/posts/{post}', [PostController::class, 'update']);
+    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+});
